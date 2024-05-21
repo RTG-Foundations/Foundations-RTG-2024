21630136ccc5a933525f7e158f03fbccffcab3ba
buddyhannah <hannahhimelright@gmail.com>
2024-05-21 15:40:32 -0600

    Updated closures.py to find transitive closure using Warshall’s Algorithm and graph relations

--- production/closures.py
+++ production/closures.py
@@ -1,3 +1,69 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+''' 
+    Displays 3D graph. Modified to work with matplotlib
+'''
+def visualize_3d(graph, positions, title, ax):
+    ax.set_title(title)
+    color_refl = 'blue'
+    color_irr = 'red'
+    color_edge = 'blue'
+    color_startrr = 'red'
+
+    # draw nodes
+    for node in graph:
+        coord = positions[node]
+        ax.scatter(coord[0], coord[1], coord[2], color=color_refl if (node, node) in graph.edges else color_irr)
+
+    # draw edges
+    for edge in graph.edges:
+        if edge[0] != edge[1]:
+            coordinates_start = positions[edge[0]]
+            coordinates_end = positions[edge[1]]
+
+            vector_start = np.array(coordinates_start)
+            vector_end = np.array(coordinates_end)
+
+            ax.plot([vector_start[0], vector_end[0]], 
+                    [vector_start[1], vector_end[1]], 
+                    [vector_start[2], vector_end[2]], color=color_edge)
+
+            vector_arrow = vector_end - vector_start
+            axis = 0.5 * vector_arrow
+            ax.quiver(vector_start[0], vector_start[1], vector_start[2],
+                      axis[0], axis[1], axis[2],
+                      color=color_startrr, length=np.linalg.norm(vector_arrow)/3)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+
+'''
+    Create  a 0-1 matrix to represent the 
+    binary relation R on Xn = {0, 1, ..., n-1}
+'''
+def getMatrix(n,R):
+    M = np.zeros((n, n)) # create 0 matrix of size nxn
+    
+    for (i,j) in R:
+        M[i][j] = 1
+
+    return M
+
+'''
+    Create a relation to represent the 0-1 matrix
+'''
+def getRelation(n, M):
+    R = set()
+    for i in range(0,n):
+        for j in range(0,n):
+            if M[i][j] == 1:
+                R.add((i,j))
+    return R
+            
 
 '''
 Input: a positive integer n; a relation R on Xn
@@ -6,14 +72,13 @@ that contains R and is reflexive (such a relation is
 called the reflexive closure of R).
 '''
 def reflexive_closure(n, R):
-    # copy R as set with original relations
-    R_prime = set(R)
-
-    # Add reflexive pairs if needed
+    M = getMatrix(n,R)
+    
+    # Mii = 1 for all i = 0, 1, ..., n-1
     for i in range(n):
-        if (i, i) not in R_prime:
-            R_prime.add((i, i))
-
+        M[i][i] = 1
+   
+    R_prime = getRelation(n,M)
     return R_prime
 
 
@@ -24,61 +89,115 @@ that contains R and is symmetric (such a relation
 is called the symmetric closure of R).
 '''
 def symmetric_closure(n, R):
-    # copy R as set with original relations
-    R_prime = set(R)
 
-    # Add pairs for symmetric
-    for (a, b) in R:
-        if (b, a) not in R_prime:
-            R_prime.add((b, a))
-
+    M = getMatrix(n,R)
+    
+    # Mij = Mji for all i = 0, 1, ..., n-1
+    for i in range(0,n):
+        for j in range(0,n):
+            if M[i][j] == 1:
+                M[j][i] = 1
+    
+    R_prime = getRelation(n,M)
     return R_prime
+
 
 '''
 Input: a positive integer n; a relation R on Xn
 Output: the transitive closure of R.
+
+https://www.geeksforgeeks.org/transitive-closure-of-a-graph/
 '''
 def transitive_closure(n, R):
-    # copy R as set with original relations
-    R_prime = set(R)
 
-    # Add pairs for transitive 
-    for (a, b) in R:
-        for (c, d) in R:
-            if b == c:
-                if (a,d) not in R_prime:
-                    R_prime.add((a,d))
-
+    M = getMatrix(n,R)
+   
+    # Floyd-Warshall algorithm for finding transitive closure
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                M[i][j] = M[i][j] or (M[i][k] and M[k][j])
+                
+    R_prime = getRelation(n,M)
     return R_prime
 
 
-'''
-Input: a positive integer n; a relation R on Xn
-Output: the connected components of (Xn, R ∪ R^(−1)).
-'''
 
 def main():
 
+    '''
+        Calculate the closures
+    '''
     n = 4
     R = {(0, 1), (1, 2), (2, 0), (3,2)}
     print(f"R: {R}\n")
 
+
     # Reflexive test
-    R_prime = reflexive_closure(n, R)
+    R_reflex = reflexive_closure(n, R)
     # R + {(0,0), (1,1), (2,2), (3,3)}
-    print(f"Reflexive closure of R: {R_prime}\n")
+    print(f"Reflexive closure of R: {R_reflex}\n")
+
 
     # Symmetric test
-    R_prime = symmetric_closure(n, R)
+    R_sym = symmetric_closure(n, R)
     # R + {(1,0), (2,1), (0,2), (2,3)}
-    print(f"Symmetric closure of R: {R_prime}\n")
+    print(f"Symmetric closure of R: {R_sym}\n")
+
 
     # Transitive test
-    R_prime = transitive_closure(n, R)
+    R_trans = transitive_closure(n, R)
     # R + {(0,2), (3,0), (1,0), (2,1)}
-    print(f"Transitive closure of R: {R_prime}\n")
+    print(f"Transitive closure of R: {R_trans}\n")
+
+   
+   
+    '''
+        Create graphs
+    '''
+
+    graph_R = nx.DiGraph()
+    for (x, y) in R:
+        graph_R.add_edge(x, y)
+
+    graph_reflexive = nx.DiGraph()
+    for (x, y) in R_reflex:
+        graph_reflexive.add_edge(x, y)
+
+    graph_symmetric = nx.DiGraph()
+    for (x, y) in R_sym:
+        graph_symmetric.add_edge(x, y)
+
+    graph_transitive = nx.DiGraph()
+    for (x, y) in R_trans:
+        graph_transitive.add_edge(x, y)
+
+   
+    # 3D positions
+    pos_R = nx.spring_layout(graph_R, dim=3)
+    pos_reflexive = nx.spring_layout(graph_reflexive, dim=3)
+    pos_symmetric = nx.spring_layout(graph_symmetric, dim=3)
+    pos_transitive = nx.spring_layout(graph_transitive, dim=3)
+
+    # Create subplots
+    fig = plt.figure(figsize=(24, 18))  
+
+    ax1 = fig.add_subplot(221, projection='3d')
+    visualize_3d(graph_R, pos_R, "R", ax1)
+
+    ax2 = fig.add_subplot(222, projection='3d')
+    visualize_3d(graph_reflexive, pos_reflexive, "Reflexive Closure of R", ax2)
+
+    ax3 = fig.add_subplot(223, projection='3d')
+    visualize_3d(graph_symmetric, pos_symmetric, "Symmetric Closure of R", ax3)
+
+    ax4 = fig.add_subplot(224, projection='3d')
+    visualize_3d(graph_transitive, pos_transitive, "Transitive Closure of R", ax4)
+
+    plt.suptitle("Red = irreflexive, Blue = reflexive")
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
-
 
